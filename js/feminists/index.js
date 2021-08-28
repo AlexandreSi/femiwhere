9630f5c5eae9678a6baa0ce48e5b87cdaaae0d1f
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2021-08-28 12:09:09 +0200

    Add Monique Wittig

--- js/feminists/index.js
+++ js/feminists/index.js
@@ -1,11 +1,13 @@
 import elenaLagadinova from "./elenaLagadinova";
 import giseleHalimi from "./giseleHalimi";
+import moniqueWittig from "./moniqueWittig";
 import roseScott from "./roseScott";
 import virginiaWoolf from "./virginiaWoolf";
 
 export default [
   elenaLagadinova,
   giseleHalimi,
+  moniqueWittig,
   roseScott,
   virginiaWoolf,
 ]
---
+++ js/feminists/moniqueWittig.js
@@ -0,0 +1,26 @@
+import Feminist from "../feminist"
+
+export default new Feminist("Monique Wittig", [
+  {
+    "latitude": 47.629288,
+    "longitude": 7.119007,
+    "title": "Dannemarie",
+    "year": 1935,
+  },
+  {
+    "latitude": 48.856396,
+    "longitude": 2.351880,
+    "title": "Paris",
+    "year": 1950,
+  },
+  {
+    "latitude": 32.223124,
+    "longitude": -110.973659,
+    "title": "Tucson",
+    "year": 2003,
+  },
+], [
+  "https://en.wikipedia.org/wiki/Monique_Wittig",
+  "La Déferlante, n°2, juin 2021",
+  "https://www.moniquewittig.com/bio/"
+])
