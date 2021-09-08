232afb7652090d1d088141b6ced9d4a4b93bccbe
AlexandreSi <32449369+AlexandreSi@users.noreply.github.com>
2021-09-08 12:00:57 +0200

    Add Simone de Beauvoir

--- js/feminists/index.js
+++ js/feminists/index.js
@@ -4,6 +4,7 @@ import giseleHalimi from "./giseleHalimi";
 import moniqueWittig from "./moniqueWittig";
 import olympeDeGouges from "./olympeDeGouges";
 import roseScott from "./roseScott";
+import simoneDeBeauvoir from "./simoneDeBeauvoir";
 import virginiaWoolf from "./virginiaWoolf";
 
 export default [
@@ -13,5 +14,6 @@ export default [
   moniqueWittig,
   olympeDeGouges,
   roseScott,
+  simoneDeBeauvoir,
   virginiaWoolf,
 ]
---
+++ js/feminists/simoneDeBeauvoir.js
@@ -0,0 +1,18 @@
+import Feminist from "../feminist"
+
+export default new Feminist("Simone de Beauvoir", [
+  {
+    "latitude": 48.847610,
+    "longitude": 2.335018,
+    "title": "Paris",
+    "year": 1908,
+  },
+  {
+    "latitude": 48.856396,
+    "longitude": 2.351880,
+    "title": "Paris",
+    "year": 1986,
+  },
+], [
+  "https://en.wikipedia.org/wiki/Simone_de_Beauvoir"
+])
